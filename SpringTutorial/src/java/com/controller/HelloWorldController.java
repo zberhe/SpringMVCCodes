901c4a2e1ec28266205d2fe3b2c136e857d2da56
Zberhe <zehaftatekea@gmail.com>
2015-12-24 11:54:53 -0500

    changed package names

--- SpringTutorial/src/java/com/controller/HelloWorldController.java
+++ SpringTutorial/src/java/com/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package net.viralpatel.spring3.controller;
+package com.controller;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
